90e122916bb481c46e21b31e11f105ff17ce729a
starikna <don.strekoza@gmail.com>
2022-08-23 17:17:33 +0500

    first commit asinc

---
+++ 7.async/task.js
@@ -0,0 +1,45 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+
+    addClock(time, callback, id) {
+        if(!id) {
+            throw new Error('Такого будильника еще нет');
+        }
+
+        if (this.alarmCollection.includes(id)) {
+            console.error('Такой звонок уже есть');
+        }    
+            this.alarmCollection.push({id, time, callback});
+    }
+
+   removeClock(id) {
+    let lengthArr1 = this.alarmCollection.length
+
+        if(this.id === id) {
+            this.alarmCollection.splice(this.alarmCollection[id], 1)
+        }
+
+        let lengthArr2 = this.alarmCollection.length
+
+        if (lengthArr1 === lengthArr2) {
+            return 0;
+        }
+
+        if (lengthArr1 !== lengthArr2) {
+            return 1;
+        }
+      }
+
+      getCurrentFormattedTime(time) {
+        return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      }
+
+      start() {
+        if (id) {
+            return 
+        }
+      }
+}
